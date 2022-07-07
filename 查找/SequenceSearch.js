efb9d23bd695bef16126c17a2df8954ade9611f2
Royin <raoyingjun@foxmail.com>
2022-07-07 11:35:22 +0800

    Update SequenceSearch.js

--- 查找/SequenceSearch.js
+++ 查找/SequenceSearch.js
@@ -1,8 +1,15 @@
-function sequenceSearch(arr, data) {
+/**
+ * @param {number[]} arr 有序数组
+ * @param  {number} element 要查找的元素
+ * @return {Number | -1} 返回查找到的元素对应索引
+ */
+function sequenceSearch(arr, element) {
     for (let i = 0, len = arr.length; i < len; i++) {
-        if (arr[i] === data) {
+        if (arr[i] === element) {
             return i
         }
     }
     return -1
 }
+
+console.log(sequenceSearch([1, 4, 7, 19, 30], 19))
