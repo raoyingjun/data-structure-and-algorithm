62f0787fd12bcb603ee651e67b3315e78220c16f
Royin <raoyingjun@foxmail.com>
2023-01-30 16:46:47 +0800

    Update BinarySearch.js

--- 查找/BinarySearch.js
+++ 查找/BinarySearch.js
@@ -7,10 +7,10 @@ function binarySearch(arr, element) {
     let low = 0,
         high = arr.length - 1
     while (low <= high) {
-        let mid = Math.ceil((low + high) / 2)
+        let mid = Math.floor((low + high) / 2)
         if (element < arr[mid]) {
             high = mid - 1
-        } else if (element >arr[mid]) {
+        } else if (element > arr[mid]) {
             low = mid + 1
         } else {
             return mid
