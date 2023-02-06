caef770263c57fadad3b2e386270331567a9cbd9
Royin <raoyingjun@foxmail.com>
2023-02-06 19:53:53 +0800

    Update BinarySearch.js

--- 查找/BinarySearch.js
+++ 查找/BinarySearch.js
@@ -4,14 +4,14 @@
  * @return {number | -1} 返回查找到的元素对应索引
  */
 function binarySearch(arr, element) {
-    let low = 0,
-        high = arr.length - 1
-    while (low <= high) {
-        let mid = Math.floor((low + high) / 2)
-        if (element < arr[mid]) {
-            high = mid - 1
-        } else if (element > arr[mid]) {
-            low = mid + 1
+    let left = 0,
+        right = arr.length - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (element > arr[mid]) {
+            left = mid + 1
+        } else if (element < arr[mid]) {
+            right = mid - 1
         } else {
             return mid
         }
